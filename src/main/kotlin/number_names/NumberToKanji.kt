c6f392e39a8da8ae7b7ea2913d6e806192407622
TomohiroSano <36667265+sanopi@users.noreply.github.com>
2019-03-07 12:13:19 +0900

    一桁の数字

--- src/main/kotlin/number_names/NumberToKanji.kt
+++ src/main/kotlin/number_names/NumberToKanji.kt
@@ -6,7 +6,21 @@ package number_names
  */
 class NumberToKanji {
 
-    fun convert(number: Int) :String {
-        return "一"
+    companion object {
+        val map = mapOf(
+                1 to "一",
+                2 to "二",
+                3 to "三",
+                4 to "四",
+                5 to "五",
+                6 to "六",
+                7 to "七",
+                8 to "八",
+                9 to "九"
+        )
+    }
+
+    fun convert(number: Int) :String? {
+        return map.get(number)
     }
 }
--- src/test/groovy/number_names/NumberToKanjiSpec.groovy
+++ src/test/groovy/number_names/NumberToKanjiSpec.groovy
@@ -1,6 +1,7 @@
 package number_names
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * created 2019/03/06
@@ -8,12 +9,21 @@ import spock.lang.Specification
  */
 class NumberToKanjiSpec extends Specification {
 
-    def "#numberを#kanjiに変換する"() {
+    @Unroll
+    def "一桁の数字(#number)を#kanjiに変換する"() {
         expect:
         new NumberToKanji().convert(number) == kanji
 
         where:
         number | kanji
         1      | "一"
+        2      | "二"
+        3      | "三"
+        4      | "四"
+        5      | "五"
+        6      | "六"
+        7      | "七"
+        8      | "八"
+        9      | "九"
     }
 }
